05d7d668b11b2d5bf7ae7a4edcf96447d6a3c714
unknown <carlos.costa.pimentel@gmail.com>
2024-10-21 19:27:59 +0100

    creating download xml script using bs4 and lxml

--- data_engineer_assessment/extract/extract.py
+++ data_engineer_assessment/extract/extract.py
@@ -2,23 +2,27 @@ from urllib import response
 from config.config import BASE_URL
 import requests
 from bs4 import BeautifulSoup
-import pandas as pd 
+import pandas as pd
 import numpy as np
+
 
 class Extract():
 
-    def __init__(self) -> None:
-        pass
-
+    def __init__(self, url) -> None:
+        self.url = BASE_URL
 
     def source_url(self):
-        url = BASE_URL
+        url = self.BASE_URL
         response = requests.get(url)
         if response.status_code == 200:
             return response
         else:
-            raise Exception(f'Failed to get data from source url ${response.status_code}')
-    
+            raise Exception(
+                f'Failed to get data from source url ${response.status_code}')
+
     def download_xml(self):
-        response = requests.get(BASE_URL)
-        soup = BeautifulSoup(response.content, 'lxml')
+        response = requests.get(self.BASE_URL)
+        if response.status_code == 200:
+            with open(BASE_URL, 'wb') as local_file:
+                soup = BeautifulSoup(response.content, 'lxml')
+                local_file.write(response.content)
